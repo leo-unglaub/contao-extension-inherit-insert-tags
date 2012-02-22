792cf1a2da4e6e60ea7fb0aa20c89aff11b9a601
Leo Unglaub <leo@leo-unglaub.net>
2012-02-22 16:05:28 +0100

    - add the english translation

---
+++ system/modules/inherit_insert_tags/languages/en/tl_page.php
@@ -0,0 +1,38 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Leo Unglaub 2012
+ * @author     Leo Unglaub <leo@leo-unglaub.net>
+ * @package    inherit_insert_tags
+ * @license    lgpl
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['inherit_insert_tags']	= array('inherit insert tags', 'Define custom insert tags and inherit them on all sub pages.');
+$GLOBALS['TL_LANG']['tl_page']['iit_name']				= array('insert tag name');
+$GLOBALS['TL_LANG']['tl_page']['iit_value']				= array('insert tag value');
+
+?>
