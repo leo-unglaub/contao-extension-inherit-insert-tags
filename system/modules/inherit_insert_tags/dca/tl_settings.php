8030c6ca900a2ddd7d9852fc5d454d6bbc891005
Leo Unglaub <leo@leo-unglaub.net>
2014-05-11 17:52:10 +0200

    Replace the old copyright notice
    Signed-off-by: Leo Unglaub <leo@leo-unglaub.net>

--- system/modules/inherit_insert_tags/dca/tl_settings.php
+++ system/modules/inherit_insert_tags/dca/tl_settings.php
@@ -1,30 +1,12 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
- * Contao Open Source CMS
- * Copyright (C) 2005-2012 Leo Feyer
+ * contao-inherit-insert-tags
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Leo Unglaub 2012
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    inherit_insert_tags
- * @license    lgpl
+ * @copyright	Leo Unglaub 2014
+ * @author		Leo Unglaub <leo@leo-unglaub.net>
+ * @package		contao-inherit-insert-tags
+ * @license		GPL
  */
 
 
